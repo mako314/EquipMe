62c70fc994955d7f30d45fcb354a81e30ccb1eb9
Macolister <bispo.mac@gmail.com>
2023-07-15 20:49:11 -0400

    work out routes, comment flow

--- server/app.py
+++ server/app.py
@@ -24,6 +24,7 @@ class UserRenters(Resource):
 
 api.add_resource(UserRenters, '/renters')
 
+#Display all Owners of Equipment who list their stuff to rent, users should be able to click the Owner and get taken to their page with all their equipment
 class EquipmentOwners(Resource):
     def get(self):
         equip_owners = [owner.to_dict() for owner in EquipmentOwner.query.all()]
@@ -35,6 +36,11 @@ class EquipmentOwners(Resource):
 
 api.add_resource(EquipmentOwners, '/equipment_owners')
 
+#this can either be ID or name,
+class EquipmentOwnersById(Resource):
+    pass
+api.add_resource(EquipmentOwnersById, '/equipment_owners/<int:id>')
+#Display all Equipment, whether available or not, this route should display all the equipment available on the website.
 class Equipments(Resource):
     def get(self):
         equipment = [equipment.to_dict() for equipment in Equipment.query.all()]
@@ -44,6 +50,7 @@ class Equipments(Resource):
         return response
 api.add_resource(Equipments, '/equipment')
 
+#Search and or filter by Equipment type, i.e. Heavy Machinery or painting
 class EquipmentByType(Resource):
     def get(self,type):
         type_test = Equipment.query.filter(Equipment.type == type).all()
@@ -60,3 +67,17 @@ class EquipmentByType(Resource):
 api.add_resource(EquipmentByType, '/equipment/<string:type>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+    #holy cow stop coding on main u gOOFY 
+
+#Routes needed -
+#1. Individual Equipment page
+#2. Rental Owner Page (So the rental owners own page with all their rental listings)
+#3. See All Rentals (Equipment)
+#4. See All owners (Owners)
+#5. Search / Filter rental by type / name / owner
+#6. See all Rentals
+#7. Checkout a rental
+#8. Post a Rental, 
+#9. Take a rental ? (Rental Agreement?)
