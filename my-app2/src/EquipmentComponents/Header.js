ab517478883647ae27d07b914b79621e8301c855
Patryk Suchecki <sucheckipatryk@gmail.com>
2023-07-19 15:49:35 -0400

    CSS for NavBar updated

--- my-app2/src/EquipmentComponents/Header.js
+++ my-app2/src/EquipmentComponents/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <img id="logo" src="https://media.istockphoto.com/id/887465766/photo/road-construction-machinery-on-the-construction-of-highway.jpg?s=612x612&w=0&k=20&c=75DpFIj4gUJ7gPodIGAcKkpS2JScsoeQLGcGRxFRgLg=" alt="EquipMe"></img>
+        <img id="logo" src="https://ali-practice-aws-bucket.s3.amazonaws.com/equipme.png" alt="EquipMe"></img>
     )
 }
 
