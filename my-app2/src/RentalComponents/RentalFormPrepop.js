faaa7874e40540d477aa32348195bcc2e911db72
Macolister <bispo.mac@gmail.com>
2023-08-16 18:44:29 -0400

    start to incorporate properties for user login functionality

--- my-app2/src/RentalComponents/RentalFormPrepop.js
+++ my-app2/src/RentalComponents/RentalFormPrepop.js
@@ -113,12 +113,11 @@ function RentalForm({ addRentalAgreement, owners, equipmentArray, featuredRental
         <label htmlFor="location" className="mb-2 inline-block text-sm text-gray-800 sm:text-base"> Location </label>
         <input type="text" name="location" value={formik.values.location} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
       </div>
-      
+
       <div className="sm:col-span-2">
         <label htmlFor="phone" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Phone</label>
         <input type="text" name="phone" value={formik.values.phone} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
       </div>
-
 
       <div className="sm:col-span-2">
         <label htmlFor="total_price" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Total Price</label>
