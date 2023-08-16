11dc1638d28b5777ff0215198a6a666655efe8cb
Macolister <bispo.mac@gmail.com>
2023-08-16 18:14:34 -0400

    fix asynchrous data in rental collection

--- my-app2/src/RentalComponents/RentalCollection.js
+++ my-app2/src/RentalComponents/RentalCollection.js
@@ -14,7 +14,10 @@ function RentalCollection({ }) {
     console.log(rentalAgreementArray)
 
     const rentalCards = rentalAgreementArray?.map((item) => {
+        console.log(item)
+        if(item && item.equipment && item.renter){
         return <RentalCard key={item.id} equipmentName={item.equipment.name} rentalDates={item.rental_dates} renterName={item.renter.name} />
+        }
     })
 
     return (<div className="cards">
