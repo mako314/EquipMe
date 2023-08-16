0f7142924bb9e8373fa0b030a303a1d077824c1c
Macolister <bispo.mac@gmail.com>
2023-08-16 18:19:47 -0400

    start on rental prepop form with tailwind

--- my-app2/src/RentalComponents/RentalFormPrepop.js
+++ my-app2/src/RentalComponents/RentalFormPrepop.js
@@ -93,17 +93,133 @@ function RentalForm({ addRentalAgreement, owners, equipmentArray, featuredRental
 
 
     return (
-        <div className="form-container">
-            <form className="form" onSubmit={formik.handleSubmit}>
-                <div className="signup-form">
+        <div className="bg-white py-6 sm:py-8 lg:py-12">
+  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+
+    <div className="mb-10 md:mb-16">
+      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Ready to List?</h2>
+
+      <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg"> Get listing in a few simple steps!</p>
+    </div>
+
+    <form onSubmit={formik.handleSubmit} className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2">
+      
+          {/* display errors from formik/yup */}
+          { formik.errors && Object.values(formik.errors).map(e => <p>{e}</p>) }
+          {/* display errors from backend */}
+          {error && <p>{error}</p>}
+      
+      <div>
+        <label htmlFor="name" className="mb-2 inline-block text-sm text-gray-800 sm:text-base"> Name </label>
+        <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+      </div>
+
+      <div className="sm:col-span-2">
+        <label htmlFor="location" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Location</label>
+        <input type="text" name="location" value={formik.values.location} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+      </div>
+
+      <div className="sm:col-span-2">
+        <label htmlFor="email" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
+        <input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+      </div>
+
+      <div className="sm:col-span-2">
+        <label htmlFor="phone" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Phone</label>
+        <input type="text" name="phone" value={formik.values.phone} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+      </div>
+
+      <div className="sm:col-span-2">
+        <label htmlFor="profession" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Profession</label>
+        <input type="text" name="profession" value={formik.values.profession} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+      </div>
+
+
+      <div className="flex items-center justify-between sm:col-span-2">
+
+        {/* NEED TO CHANGE COLOR */}
+        <button type="submit" className="inline-block rounded-lg bg-orange-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"> Sign Up!</button>
+
+        <span className="text-sm text-gray-500">*Required</span>
+      </div>
+    </form>
+
+  </div>
+</div>
+        
+    )
+}
+
+export default RentalForm;
+
+
+// {/* <div className="bg-white py-6 sm:py-8 lg:py-12">
+//   <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+
+//     <div className="mb-10 md:mb-16">
+//       <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Ready to List?</h2>
+
+//       <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg"> Get listing in a few simple steps!</p>
+//     </div>
+
+//     <form onSubmit={formik.handleSubmit} className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2">
+      
+//           {/* display errors from formik/yup */}
+//           { formik.errors && Object.values(formik.errors).map(e => <p>{e}</p>) }
+//           {/* display errors from backend */}
+//           {error && <p>{error}</p>}
+      
+//       <div>
+//         <label htmlFor="name" className="mb-2 inline-block text-sm text-gray-800 sm:text-base"> Name </label>
+//         <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+//       </div>
+
+//       <div className="sm:col-span-2">
+//         <label htmlFor="location" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Location</label>
+//         <input type="text" name="location" value={formik.values.location} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+//       </div>
+
+//       <div className="sm:col-span-2">
+//         <label htmlFor="email" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
+//         <input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+//       </div>
+
+//       <div className="sm:col-span-2">
+//         <label htmlFor="phone" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Phone</label>
+//         <input type="text" name="phone" value={formik.values.phone} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+//       </div>
+
+//       <div className="sm:col-span-2">
+//         <label htmlFor="profession" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Profession</label>
+//         <input type="text" name="profession" value={formik.values.profession} onChange={formik.handleChange} className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+//       </div>
+
+
+//       <div className="flex items-center justify-between sm:col-span-2">
+
+//         {/* NEED TO CHANGE COLOR */}
+//         <button type="submit" className="inline-block rounded-lg bg-orange-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"> Sign Up!</button>
+
+//         <span className="text-sm text-gray-500">*Required</span>
+//       </div>
+//     </form>
+
+//   </div>
+// </div> */}
+
+
+
+// <div className="form-container">
+//             <form className="form" onSubmit={formik.handleSubmit}>
+//                 <div className="signup-form"> */}
 
                     {/* display errors from formik/yup */}
-                    {formik.errors && Object.values(formik.errors).map(e => <p>{e}</p>)}
+                    {/* {formik.errors && Object.values(formik.errors).map(e => <p>{e}</p>)} */}
 
                     {/* display errors from backend */}
-                    {error && <p>{error}</p>}
+                    {/* {error && <p>{error}</p>} */}
 
-                    <div className="submit-form">
+                    {/* <div className="submit-form">
                         <label>Location: </label>
                         <input
                             type="text"
@@ -170,8 +286,4 @@ function RentalForm({ addRentalAgreement, owners, equipmentArray, featuredRental
                 </div>
                 <input type='submit' className="submit-btn" />
             </form>
-        </div>
-    )
-}
-
-export default RentalForm;
+        </div> */}
