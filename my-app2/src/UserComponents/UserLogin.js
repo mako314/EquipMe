9b9dd48c93c66ad2f21ccec67c0fa5dc852b055b
Macolister <bispo.mac@gmail.com>
2023-08-17 17:35:27 -0400

    add routing for userLogin

--- my-app2/src/UserComponents/UserLogin.js
+++ my-app2/src/UserComponents/UserLogin.js
@@ -1,8 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
 function UserLogin(){
+
+    const [user, setUser] = useState(null); // stores user on client-side
+
+    console.log(user);
+
+    // grabs current session from server-side and sets state
+    function handleCheckSession() {
+        fetch("/check_session").then((resp) => {
+          if (resp.ok) {
+            resp.json().then((user) => setUser(user));
+          }
+        });
+    }
+
+    // sends information to server-side, sets session, and sets state
+    function handleLogin(e) {
+        e.preventDefault();
+
+        let username = e.target.username.value;
+
+        fetch("/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify( { username } ),
+          }).then((resp) => {
+            if (resp.ok) {
+              resp.json().then((user) => setUser(user));
+            }
+          });
+    }
+
+    // removes session, removes state
+    function handleLogout() {
+        fetch("/logout", {
+            method: "DELETE"
+        }).then(setUser(null))
+    }
 
 
     return(
-        <>
+    <>
     <h1>Login Form</h1>
     <form onSubmit = {handleLogin}>
         <label>Username: </label>
@@ -21,3 +62,4 @@ function UserLogin(){
     )
 }
 
+export default UserLogin;
