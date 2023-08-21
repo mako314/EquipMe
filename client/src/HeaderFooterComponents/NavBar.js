59a908f04615c3e8a5f2eae3a35a62070f406da1
Macolister <bispo.mac@gmail.com>
2023-08-20 22:32:25 -0400

    new color for nav bar, change first color section

--- client/src/HeaderFooterComponents/NavBar.js
+++ client/src/HeaderFooterComponents/NavBar.js
@@ -27,7 +27,7 @@ function NavBar({ setSearchTerm }) {
     return (
         <>
       {/*<!-- Component: Basic Navbar --> */}
-      <header className="border-b-1 relative z-20 w-full border-b border-slate-200 bg-amber-500/90 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">        <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
+      <header className="border-b-1 relative z-20 w-full border-b border-slate-200 bg-gray-900 dark:bg-gray-900 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">        <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
           <nav
             aria-label="main navigation"
             className="flex h-[5.5rem] items-stretch justify-between font-medium text-slate-700"
