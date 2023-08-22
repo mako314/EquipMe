5a6fdf162eeaf8849f721a9310d0fc5a4c4647da
Macolister <bispo.mac@gmail.com>
2023-08-22 17:26:20 -0400

    add new hero with pictures

--- client/src/HomePageComponents/HomePage.js
+++ client/src/HomePageComponents/HomePage.js
@@ -186,18 +186,18 @@ function HomePage({ equipmentArray, setFeaturedRental }) {
   <section class="mx-auto max-w-screen-2xl px-4 md:px-8">
     <div class="mb-8 flex flex-wrap justify-between md:mb-16">
       <div class="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
-        <h1 class="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">Find your<br />style online</h1>
+        <h1 class="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">EquipMe <br/>Connect.<br/> Rent.<br/> Prosper.</h1>
 
-        <p class="max-w-md leading-relaxed text-gray-500 xl:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares characteristics of real text.</p>
+        <p class="max-w-md leading-relaxed text-gray-500 xl:text-lg">Unleash the potential of your projects with EquipMe's collection of top-tier tools and equipment. Your one-stop destination for gear that's as passionate about your projects as you are.</p>
       </div>
 
       <div class="mb-12 flex w-full md:mb-16 lg:w-2/3">
         <div class="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
-          <img src="https://images.unsplash.com/photo-1542340916-951bb72c8f74?auto=format&q=75&fit=crop&w=550&h=550" loading="lazy" alt="Photo by Kaung Htet" class="h-full w-full object-cover object-center" />
+          <img src="https://media.istockphoto.com/id/143918313/photo/excavator-at-a-construction-site-against-the-setting-sun.jpg?s=612x612&w=0&k=20&c=1ULa8wwAxgczZDRpmVYuR-cC7wTpIWSZMzVhOCOgjr0=" loading="lazy" alt="Creator: Avalon_Studio | Credit: Getty Images Copyright: small_frog" class="h-full w-full object-cover object-center" />
         </div>
 
         <div class="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
-          <img src="https://images.unsplash.com/photo-1586295166487-b265f7e83a7f?auto=format&q=75&fit=crop&w=550&h=550" loading="lazy" alt="Photo by Manny Moreno" class="h-full w-full object-cover object-center" />
+          <img src="https://www.deadlinenews.co.uk/wp-content/uploads/2022/09/zac-edmonds-N1LBcqLP9ec-unsplash-1024x683.jpg" loading="lazy" alt="Photo by Zac Edmonds on Unsplash" class="h-full w-full object-cover object-center" />
         </div>
       </div>
     </div>
