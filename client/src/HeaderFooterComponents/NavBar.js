aace456cc91f8bcbb15427e732a3cc6f1bc49932
Macolister <bispo.mac@gmail.com>
2023-08-23 17:11:17 -0400

    adjust button placement for lIdisplay navbar

--- client/src/HeaderFooterComponents/NavBar.js
+++ client/src/HeaderFooterComponents/NavBar.js
@@ -158,13 +158,9 @@ function NavBar({ setSearchTerm }) {
 
             </ul>
             
-            <div className="ml-auto flex items-center px-6 lg:ml-0 lg:p-0">
-              <button className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-amber-500 px-5 text-sm font-medium tracking-wide text-white shadow-md shadow-amber-200 transition duration-300 hover:bg-emerald-600 hover:shadow-sm hover:shadow-emerald-200 focus:bg-emerald-700 focus:shadow-sm focus:shadow-emerald-200 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-                <Link to='/login'
-                onClick={handleLogout} // Close the mobile view when link is clicked
-                >
-                <span>Logout</span>
-                </Link>
+            <div className="ml-auto flex items-center px-6 lg:ml-0 lg:pl-3">
+              <button className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-amber-500 px-5 text-sm font-medium tracking-wide text-white shadow-md shadow-amber-200 transition duration-300 hover:bg-emerald-600 hover:shadow-sm hover:shadow-emerald-200 focus:bg-emerald-700 focus:shadow-sm focus:shadow-emerald-200 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none" onClick={handleLogout}>
+              Logout
               </button>
             </div>
 
