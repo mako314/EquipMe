425bab97fbdd2903e1694e41c7a5fc2c0cf07258
Macolister <bispo.mac@gmail.com>
2023-08-23 15:20:16 -0400

    spacing in search placeholder, footer less links

--- client/src/HeaderFooterComponents/Footer.js
+++ client/src/HeaderFooterComponents/Footer.js
@@ -9,25 +9,26 @@ function Footer() {
         <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
         <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
         <EquipMeLogo className="h-12 w-12" />
-            <span class="ml-3 text-xl">Tailblocks</span>
+            <span class="ml-3 text-xl">EquipMe</span>
         </a>
-        <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+        <p class="mt-2 text-sm text-white">Connect. Rent. Prosper</p>
         </div>
-        <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+        {/* justify can adjust where the text is located */}
+        <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center justify-center">
         <div class="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
             <nav class="list-none mb-10">
             <li>
-                <a class="text-white hover:text-gray-800">First Link</a>
+                <a class="text-white hover:text-amber-500">First Link</a>
             </li>
             <li>
-                <a class="text-white hover:text-gray-800">Second Link</a>
+                <a class="text-white hover:text-amber-500">Second Link</a>
             </li>
             <li>
-                <a class="text-white hover:text-gray-800">Third Link</a>
+                <a class="text-white hover:text-amber-500">Third Link</a>
             </li>
             <li>
-                <a class="text-white hover:text-gray-800">Fourth Link</a>
+                <a class="text-white hover:text-amber-500">Fourth Link</a>
             </li>
             </nav>
         </div>
@@ -35,53 +36,20 @@ function Footer() {
             <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
             <nav class="list-none mb-10">
             <li>
-                <a class="text-white hover:text-gray-800">First Link</a>
+                <a class="text-white hover:text-amber-500">First Link</a>
             </li>
             <li>
-                <a class="text-white hover:text-gray-800">Second Link</a>
+                <a class="text-white hover:text-amber-500">Second Link</a>
             </li>
             <li>
-                <a class="text-white hover:text-gray-800">Third Link</a>
+                <a class="text-white hover:text-amber-500">Third Link</a>
             </li>
             <li>
-                <a class="text-white hover:text-gray-800">Fourth Link</a>
+                <a class="text-white hover:text-amber-500">Fourth Link</a>
             </li>
             </nav>
         </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-            <nav class="list-none mb-10">
-            <li>
-                <a class="text-white hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-                <a class="text-white hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-                <a class="text-white hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-                <a class="text-white hover:text-gray-800">Fourth Link</a>
-            </li>
-            </nav>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-            <nav class="list-none mb-10">
-            <li>
-                <a class="text-white hover:text-gray-800">First Link</a>
-            </li>
-            <li>
-                <a class="text-white hover:text-gray-800">Second Link</a>
-            </li>
-            <li>
-                <a class="text-white hover:text-gray-800">Third Link</a>
-            </li>
-            <li>
-                <a class="text-white hover:text-gray-800">Fourth Link</a>
-            </li>
-            </nav>
-        </div>
+
         </div>
     </div>
     <div class="bg-gray-100">
