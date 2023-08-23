28ad737332d50aaa15818fa8a26f13ca49dfa34d
Macolister <bispo.mac@gmail.com>
2023-08-23 15:31:26 -0400

    working route to user profile

---
+++ client/src/UserComponents/UserProfile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function UserProfile(){
+    
+    
+    
+    
+    
+    
+    
+    
+return (
+    <>
+    <div> <p>Hello, you've reached the page </p> </div>
+    
+        
+    </>
+)
+
+
+}
+
+export default UserProfile;
