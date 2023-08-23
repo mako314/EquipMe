803a7530e375eaa67ad1ed2d9e865cbaa59c14c9
Macolister <bispo.mac@gmail.com>
2023-08-23 15:03:46 -0400

    remove header, start on footer to make clean display page

---
+++ client/src/HeaderFooterComponents/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <img id="logo" src="https://ali-practice-aws-bucket.s3.amazonaws.com/equipme.png" alt="EquipMe"></img>
+    )
+}
+
+export default Footer;
