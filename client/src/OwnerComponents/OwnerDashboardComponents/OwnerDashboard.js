203028988eab283055ed0b1db1ca7daa30e178aa
Macolister <bispo.mac@gmail.com>
2023-08-30 19:33:08 -0400

    logo links to homepage, need to edit styling

--- client/src/OwnerComponents/OwnerDashboardComponents/OwnerDashboard.js
+++ client/src/OwnerComponents/OwnerDashboardComponents/OwnerDashboard.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import OwnerContext from '../OwnerContext';
 import {ReactComponent as EquipMeLogo } from '../../Content/EquipMeLogo.svg'
@@ -34,9 +34,11 @@ function OwnerDashboard() {
 
       {/* <!-- Component Start --> */}
               <div className="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-300">
-                  <span className="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300" href="#">
+                  <Link to='/'>
+                  <span className="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300">
                       <EquipMeLogo/>
                   </span>
+                  </Link>
                   <a className="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300" href="#">
                       <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
