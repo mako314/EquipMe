6cc38eb8ba15235f5dd1087ee1187f2dc5814e6a
Macolister <bispo.mac@gmail.com>
2023-08-30 19:13:14 -0400

    nice starting skeleton for div sizes

--- client/src/OwnerComponents/OwnerDashboardComponents/OwnerDashboard.js
+++ client/src/OwnerComponents/OwnerDashboardComponents/OwnerDashboard.js
@@ -114,30 +114,15 @@ function OwnerDashboard() {
                         </div>
                         <div class="flex-grow p-6 overflow-auto bg-gray-200">
                             <div class="grid grid-cols-3 gap-6">
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
                                 <div class="h-24 col-span-3 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-3 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
+                                <div class="h-96 col-span-1 bg-white border border-gray-300"></div>
+                                <div class="h-96 col-span-1 bg-white border border-gray-300"></div>
+                                <div class="h-96 col-span-1 bg-white border border-gray-300"></div>
+                                <div class="h-96 col-span-2 bg-white border border-gray-300"></div>
+                                <div class="h-96 col-span-1 bg-white border border-gray-300"></div>
                                 <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
                                 <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                                <div class="h-24 col-span-3 bg-white border border-gray-300"></div>
+                                
                             </div>
                         </div>
                     </div>
