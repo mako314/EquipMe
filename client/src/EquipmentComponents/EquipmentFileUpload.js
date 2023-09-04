cc61b554e98d1135cc44a167764f9e920d5ae2ad
Macolister <bispo.mac@gmail.com>
2023-09-04 16:51:01 -0400

    install firebase, start on file upload

---
+++ client/src/EquipmentComponents/EquipmentFileUpload.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function EquipmentFileUpload() {
+
+    const navigate = useNavigate();
+
+    return (
+
+        <div className="">
+            <input type="file"/>
+             
+        </div>
+
+
+    )
+}
+
+export default EquipmentFileUpload;
