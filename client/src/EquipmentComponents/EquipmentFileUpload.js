c0c1f2f39bc0953df5333c7a06cdc3ea0eb02e7d
Macolister <bispo.mac@gmail.com>
2023-09-04 17:26:02 -0400

    succesfully upload images to firebase

--- client/src/EquipmentComponents/EquipmentFileUpload.js
+++ client/src/EquipmentComponents/EquipmentFileUpload.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { storage } from "../CloudComponents/Firebase";
+import { ref, uploadBytes } from 'firebase/storage';
+import {v4} from 'uuid';
 
 function EquipmentFileUpload() {
 
     const navigate = useNavigate();
-
+    
+    const [imageUpload, setImageUpload] = useState(null)
     const uploadImage = () => {
-
+        if (imageUpload == null) return;
+        const imageRef = ref(storage, `equipmentImages/${imageUpload.name + v4()}`);
+        uploadBytes(imageRef, imageUpload).then(() =>{
+            alert("Image Uploaded!")
+        })
     }
 
     return (
 
         <div className="">
-            <input type="file"/>
+            <input type="file" onChange={
+                (event) => {setImageUpload(event.target.files[0])
+                }}/>
              <button onClick={uploadImage}> Upload Image </button>
         </div>
 
