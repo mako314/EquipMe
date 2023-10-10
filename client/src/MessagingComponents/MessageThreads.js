64e39c695198ba20f94a925ab799504a46b17fc3
Macolister <bispo.mac@gmail.com>
2023-10-09 22:19:18 -0400

    basic styling for messaging portion

--- client/src/MessagingComponents/MessageThreads.js
+++ client/src/MessagingComponents/MessageThreads.js
@@ -17,19 +17,25 @@ function MessageThreads() {
   }, [])
 
   return (
-    <div>
-      <h2>Message Threads</h2>
-      <ul>
-        {threads.map((thread) => (
-        <div> 
-          <li key={thread.id}><strong>{thread.subject}</strong></li>
-          <br></br>
-          <li> {thread.content} </li>
-        </div>
-        ))}
-      </ul>
+    <div className="bg-gray-100 min-h-screen">
+      <div className="container mx-auto p-4">
+        <ul>
+          {threads.map((thread) => (
+            <> 
+            <h2 className="text-2xl font-bold mb-4">{thread.subject}</h2>
+            <div
+              key={thread.id}
+              className="bg-white rounded-lg shadow-md p-4 mb-4"
+            >
+              <p className="text-lg font-semibold mb-2"></p>
+              <p className="text-gray-600">{thread.content}</p>
+            </div>
+            </>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default MessageThreads;
