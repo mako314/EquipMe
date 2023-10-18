bfc28c5c361fa903add01903cc846c345a69d202
Macolister <bispo.mac@gmail.com>
2023-10-17 23:01:21 -0400

    make a route for user messages, two routes now for messaging

--- server/app.py
+++ server/app.py
@@ -785,7 +785,45 @@ class OwnerMessages(Resource):
             }, 404)
             return response
 
-api.add_resource(OwnerMessages, "/messages/<int:owner_id>/")
+api.add_resource(OwnerMessages, "/owner/messages/<int:owner_id>/")
+
+class UserMessages(Resource):
+    def get(self, user_id):
+        owner_message_threads = Inbox.query.filter_by(user_id=user_id).all()
+        message_threads = []
+        # print(owner_message_threads)
+
+        for thread in owner_message_threads:
+            # Needed to change this to just ID as opposed to message_id
+            message_id = thread.id
+            # Use message_id to retrieve the content of the message, sender, and recipient
+            message = Message.query.get(message_id)
+            sender_id = message.sender_id
+            context_id = message.context_id
+            recipient_id = message.recipient_id
+            subject = message.subject
+            content = message.content
+            
+            # Append the message details to the message_threads list
+            message_threads.append({
+                "id": message_id,
+                "message_id": message_id,
+                "sender_id": sender_id,
+                "recipient_id": recipient_id,
+                "context_id": context_id,
+                "subject": subject,
+                "content": content
+            })
+
+        if message_threads:
+            return make_response(message_threads, 200)
+        else:
+            response = make_response({
+            "error": "Message thread not found"
+            }, 404)
+            return response
+
+api.add_resource(UserMessages, "/user/messages/<int:user_id>/")
 
 class SendMessage(Resource):
     def post(self):
