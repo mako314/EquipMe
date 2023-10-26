183a018486d152421f3b82bee9f7f6e29b2098b9
Macolister <bispo.mac@gmail.com>
2023-10-26 16:32:47 -0400

    center modal

--- client/src/UserComponents/UserCard.js
+++ client/src/UserComponents/UserCard.js
@@ -28,9 +28,9 @@ function UserCard({ id, email, name, location, phone, profileImage, item, profes
         id="authentication-modal" 
         tabIndex="-1" 
         aria-hidden="true" 
-        className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center overflow-x-hidden overflow-y-auto"
     >
-        <div className="relative w-full max-w-md max-h-full">
+        <div className="relative w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 
                 <button 
