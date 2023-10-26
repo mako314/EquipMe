83dedf9335c8add8e07bb773c75d8fd7c276caf1
Macolister <bispo.mac@gmail.com>
2023-10-26 16:21:03 -0400

    modal in owner dash for contacting, start

--- client/src/UserComponents/UserCard.js
+++ client/src/UserComponents/UserCard.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom"
 
 function UserCard({ id, email, name, location, phone, profileImage, item, profession }) {
 
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     //test can likely delete below
     // console.log(item)
@@ -12,6 +13,75 @@ function UserCard({ id, email, name, location, phone, profileImage, item, profes
     function handleClick(e) {
         navigate(`/equipment_owners/${id}`)
     }
+
+    function toggleModal() {
+        setIsModalOpen(!isModalOpen)
+    }
+
+    let contactButton = 
+    <> 
+    <button onClick={toggleModal} className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+    Contact
+    </button>
+    {isModalOpen && (
+    <div 
+        id="authentication-modal" 
+        tabIndex="-1" 
+        aria-hidden="true" 
+        className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    >
+        <div className="relative w-full max-w-md max-h-full">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                
+                <button 
+                    onClick={toggleModal} 
+                    type="button" 
+                    className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                    <span className="sr-only">Close modal</span>
+                </button>
+
+                <div className="px-6 py-6 lg:px-8">
+                    <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+                    <form className="space-y-6" action="#">
+                        
+                        <div>
+                        <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Subject: </label>
+                        <input type="subject" name="subject" id="subject" placeholder="Subject" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                        </div>
+                        
+                        <div>
+                            {/* Message fields here */}
+                        </div>
+
+                        <div className="flex justify-between">
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    {/* Checkbox*/}
+                                </div>
+                                <label 
+                                    htmlFor="remember" 
+                                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                >
+                                    Could include something extra here, 
+                                </label>
+                            </div>                        </div>
+                        <button 
+                            type="submit" 
+                            className=" w-half text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Send Message
+                        </button>
+                        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                            testing what this looks like  
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+)}
+</>
 
     // <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={() => handleOwnerDelete(item)}>Delete Owner</button>
     // <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={() => handleEditOwner(item)}>Edit Owner</button>
@@ -31,7 +101,7 @@ function UserCard({ id, email, name, location, phone, profileImage, item, profes
                     </div>
                     
                         <button className= " ml-auto text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handleClick}>More Info</button>
-                    
+                        {contactButton}
                 </div>
             </div>
         </div>
