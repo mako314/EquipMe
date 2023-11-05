b6d14fe45d3780c3bc5520fe9920d4fafe1c8906
Macolister <bispo.mac@gmail.com>
2023-11-05 17:25:15 -0500

    forgot use context in app silly me

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate, Route, Routes } from 'react-router-dom';
 
 import './App.css';
