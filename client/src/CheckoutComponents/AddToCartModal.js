95427e5641700124a6b323cf2c2442d5a7c45a0d
Macolister <bispo.mac@gmail.com>
2023-11-07 12:38:49 -0500

    comment out modal for add to cart, swap branches

--- client/src/CheckoutComponents/AddToCartModal.js
+++ client/src/CheckoutComponents/AddToCartModal.js
@@ -2,26 +2,26 @@ import React from "react";
 
 function AddToCartModal(){
     
-  function handleAddToCartClick() {
+//   function handleAddToCartClick() {
         
-    let email = formik.values.email;
-    let password = formik.values.password;
+//     let email = formik.values.email;
+//     let password = formik.values.password;
 
-    fetch(`${apiUrl}cart/${id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify( { email, password } ),
-      }).then((resp) => {
-        if (resp.ok) {
-          resp.json().then((user) => {
-            setUser(user)
-            navigate(`/user/profile/${user.id}`); // <-------- navigates to the profile
-          });
-        }
-      });
-}
+//     fetch(`${apiUrl}cart/${id}`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify( { email, password } ),
+//       }).then((resp) => {
+//         if (resp.ok) {
+//           resp.json().then((user) => {
+//             setUser(user)
+//             navigate(`/user/profile/${user.id}`); // <-------- navigates to the profile
+//           });
+//         }
+//       });
+// }
 
 
     return(
