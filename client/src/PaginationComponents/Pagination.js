3546e7e987268cd4dc2e85c84d1c5b07f553053a
Macolister <bispo.mac@gmail.com>
2023-11-12 21:19:41 -0500

    add styling to pagination WIP

--- client/src/PaginationComponents/Pagination.js
+++ client/src/PaginationComponents/Pagination.js
@@ -7,18 +7,16 @@ function Pagination({ paginate, array, cardsPerPage, pageCount }) {
     
     return (
         <ReactPaginate
-            className="pagination"
             onPageChange={paginate}
             pageCount={pageCount}
-            previousLabel={"≪ previous"}
-            pageRangeDisplayed={1}
+            previousLabel={"≪  previous"}
             nextLabel={"next ≫"}
             breakLabel="..."
-            containerClassName={"pagination"}
-            pageLinkClassName={"page-number"}
-            previousLinkClassName={"page-number"}
-            nextLinkClassName={"page-number"}
-            activeLinkClassName={"active"}
+            containerClassName={"flex list-none items-center justify-center divide-x divide-slate-200 overflow-hidden rounded border border-slate-200 text-sm text-slate-700 mt-10"} 
+            pageClassName={"hidden md:inline-flex h-10 items-center justify-center stroke-slate-700 px-4 text-sm font-medium text-slate-700 transition duration-300 hover:bg-emerald-50 hover:text-emerald-500 focus:bg-emerald-50 focus:text-emerald-600 focus-visible:outline-none"}
+            previousClassName={"page-number px-2"}
+            nextClassName={"page-number px-2"}
+            activeLinkClassName={"bg-emerald-500 text-white px-3"}
         />
     );
 }
