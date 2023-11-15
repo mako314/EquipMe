040d5d46b820615961853cbe0b463206c2a12e5c
Macolister <bispo.mac@gmail.com>
2023-11-15 14:45:52 -0500

    uninstall react-calendar, use input instead

--- client/src/CalendarComponents/Calendar.js
+++ client/src/CalendarComponents/Calendar.js
@@ -1,45 +1,51 @@
 import { useState } from 'react';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-function App() {
-  const [date, setDate] = useState(new Date());
-  console.log(date)
+function Calendar() {
+
+  // This will handle the first input / Start Date
+
+  // This gives too much info to be able to pass it to default type, 
+  let startDate = new Date();
+  // Get timezone offset in minutes and convert it to milliseconds
+  let timezoneOffset = startDate.getTimezoneOffset() * 60000
+  // Adjust the date to local timezone
+  let localDate = new Date(startDate - timezoneOffset)
+  // Format the date to "YYYY-MM-DDTHH:mm" format
+  let formattedDate = localDate.toISOString().slice(0, 16)
+
+  const addDays = (date, days) => {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+    
+    let resultTimezone = result.getTimezoneOffset() * 60000
+
+    let localResult = new Date(result - resultTimezone)
+
+    let formattedResult = localResult.toISOString().slice(0, 16)
+
+    return formattedResult
+  }
+
+  console.log("Starting day:", formattedDate)
+  console.log("One year from now:",addDays(formattedDate, 365))
+
+
   return (
     <div className='app'>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar 
-        onChange={setDate} 
-        value={date} 
-        selectRange={true}
-        hover={new Date(2017, 0, 1)}
-        minDate={new Date()}
+      <label for="meeting-time">Choose a time for your appointment:</label>
+
+      <input
+        type="datetime-local"
+        id="meeting-time"
+        name="meeting-time"
+        // value={formattedDate}
+        min = {formattedDate}
+        max={addDays(formattedDate, 365)}
         />
-      </div>
-      <p className='text-center'>
-      {date.length > 0 ? (
-        <p className='text-center'>
-          <span className='bold'>Start:</span> {date[0].toDateString()}
-          <p>  </p>
-          <span className='bold'>End:</span> {date[1].toDateString()}
-        </p>
-      ) : (
-        <p className='text-center'>
-          <span className='bold'>Default selected date:</span>{' '}
-          {date.toDateString()}
-        </p>
-      )}
-      
-        {/* {date?.map((item) => {
-          return (<span className='bold'>Selected Date: {item.toDateString()} </span>)
-        })} */}
-        
-        {console.log(date)}
-        {/* {date.toDateString()} */}
-      </p>
+
     </div>
   );
 }
 
-export default App;
+export default Calendar;
