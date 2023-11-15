ccd55a66c81b26fd4c784e41c99e19801288b6f3
Macolister <bispo.mac@gmail.com>
2023-11-15 14:53:17 -0500

    input now has calculated times

--- client/src/CalendarComponents/Calendar.js
+++ client/src/CalendarComponents/Calendar.js
@@ -1,18 +1,26 @@
 import { useState } from 'react';
-import 'react-calendar/dist/Calendar.css';
 
 function Calendar() {
 
   // This will handle the first input / Start Date
 
   // This gives too much info to be able to pass it to default type, 
-  let startDate = new Date();
+  let startDate = new Date()
+  console.log(typeof(startDate))
+  console.log(startDate)
+  let endDate = new Date()
+
   // Get timezone offset in minutes and convert it to milliseconds
-  let timezoneOffset = startDate.getTimezoneOffset() * 60000
+  let timezoneOffset = startDate.getTimezoneOffset() * 60000 // EST
+
   // Adjust the date to local timezone
-  let localDate = new Date(startDate - timezoneOffset)
-  // Format the date to "YYYY-MM-DDTHH:mm" format
-  let formattedDate = localDate.toISOString().slice(0, 16)
+  let localStartDate = new Date(startDate - timezoneOffset)
+  let localEndDate = new Date((endDate - timezoneOffset) + 3600000)
+
+  // Format the date to "YYYY-MM-DDTHH:mm" format as required by the input :cry:
+  let formattedStartDate = localStartDate.toISOString().slice(0, 16)
+  //End
+  let formattedEndDate = localEndDate.toISOString().slice(0, 16)
 
   const addDays = (date, days) => {
     let result = new Date(date);
@@ -27,21 +35,24 @@ function Calendar() {
     return formattedResult
   }
 
-  console.log("Starting day:", formattedDate)
-  console.log("One year from now:",addDays(formattedDate, 365))
+  console.log("Starting day:", formattedStartDate)
+  console.log("One year from now:",addDays(formattedStartDate, 365))
+
+  console.log("End date should be +1 hr:", formattedEndDate)
+  console.log("One year from now:",addDays(formattedEndDate, 365))
 
 
   return (
-    <div className='app'>
-      <label for="meeting-time">Choose a time for your appointment:</label>
+    <div className='Calendar'>
+      <label for="meeting-time">Choose a starting day and time for your rental:  </label>
 
       <input
         type="datetime-local"
         id="meeting-time"
         name="meeting-time"
         // value={formattedDate}
-        min = {formattedDate}
-        max={addDays(formattedDate, 365)}
+        min = {formattedStartDate}
+        max={addDays(formattedStartDate, 365)}
         />
 
     </div>
