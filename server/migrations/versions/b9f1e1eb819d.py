ec1879fc059075fe94a729676968a5c960600a80
Macolister <bispo.mac@gmail.com>
2023-11-19 21:07:01 -0500

    revision with missing version

---
+++ server/migrations/versions/b9f1e1eb819d.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fa46f76e8cd1
+Revises: a757c524f05e
+Create Date: 2023-11-19 21:06:16.845788
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa46f76e8cd1'
+down_revision = 'a757c524f05e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
