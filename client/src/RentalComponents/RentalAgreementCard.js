867612b726b1615901a6ee867b740754814d7f5e
Macolister <bispo.mac@gmail.com>
2023-11-24 16:40:11 -0500

    rental agreements looking good, going to work on status

--- client/src/RentalComponents/RentalAgreementCard.js
+++ client/src/RentalComponents/RentalAgreementCard.js
@@ -15,7 +15,7 @@ function RentalAgreementCard({ equipmentName, rentalStart, rentalEnd, renterFirs
         </div>
 
         <div className="px-6 py-4">
-            <h1 className="text-xl font-semibold text-gray-800 dark:text-white">{ownerFirstName} {ownerLastName} </h1>
+            <h1 className="text-xl font-semibold text-gray-800 dark:text-white"> {renterFirstName} {renterLastName}  </h1>
 
             <p className="py-2 text-gray-700 dark:text-gray-400"> {rentalStart} to {rentalEnd}</p>
 
@@ -24,7 +24,7 @@ function RentalAgreementCard({ equipmentName, rentalStart, rentalEnd, renterFirs
                     <path d="M14 11H10V13H14V11Z"/><path fillRule="evenodd" clipRule="evenodd" d="M7 5V4C7 2.89545 7.89539 2 9 2H15C16.1046 2 17 2.89545 17 4V5H20C21.6569 5 23 6.34314 23 8V18C23 19.6569 21.6569 21 20 21H4C2.34314 21 1 19.6569 1 18V8C1 6.34314 2.34314 5 4 5H7ZM9 4H15V5H9V4ZM4 7C3.44775 7 3 7.44769 3 8V14H21V8C21 7.44769 20.5522 7 20 7H4ZM3 18V16H21V18C21 18.5523 20.5522 19 20 19H4C3.44775 19 3 18.5523 3 18Z"/>
                 </svg>
 
-                <h1 className="px-2 text-sm"> Renter : {renterFirstName} {renterLastName}</h1>
+                <h1 className="px-2 text-sm"> Owner : {ownerFirstName} {ownerLastName}</h1>
             </div>
 
             <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
