7406ffbd29ab0b397edc1ba80b80d6934e28a3d3
Macolister <bispo.mac@gmail.com>
2023-11-28 16:39:05 -0500

    add format for rental days in collection

--- client/src/RentalComponents/RentalAgreementsCollection.js
+++ client/src/RentalComponents/RentalAgreementsCollection.js
@@ -16,22 +16,14 @@ function RentalAgreementsCollection({ }) {
       console.error('Invalid date provided to formatDate:', newDate)
       return null
     }
-
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
     let options = {
       year: 'numeric', month: '2-digit', day: '2-digit',
       hour: '2-digit', minute: '2-digit',
       hour12: true
     }
 
-    // Adjust the date to local timezone, .getTimeZoneOffset returns the difference in minutes. Most of this needs second / milliseconds so that's where the *60000 comes from
-    let timezoneOffset = newDate.getTimezoneOffset() * 60000
-    // To get the local date, you just take the date z(which we test to see if it was an instance of Date, since the useEffect makes it so it continues). When subtracting timezoneOffset, you just get the appropriate time, 24hr format.
-    let localDate = new Date(newDate - timezoneOffset)
-    // Slice so I don't get the very long info, can do -8 or 16. 
-    // https://stackoverflow.com/questions/67423091/react-jsx-add-min-date-time-to-datetime-local-input
-    // For some reason though my MIN did NOT work
-    console.log(localDate)
-    return localDate.toLocaleDateString('en-US', options)
+    return newDate.toLocaleDateString('en-US', options)
   }
 
 
