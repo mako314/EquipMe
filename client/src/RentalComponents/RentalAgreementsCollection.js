3f1c68da075e8d8c1f55b5f3240ac9622a944671
Macolister <bispo.mac@gmail.com>
2023-12-12 00:25:45 -0500

    owner rental agreements also can be sorted

--- client/src/RentalComponents/RentalAgreementsCollection.js
+++ client/src/RentalComponents/RentalAgreementsCollection.js
@@ -10,14 +10,21 @@ function RentalAgreementsCollection({ setFromOwnerDash, fromOwnerDash, agreement
     const [sortedCards, setSortedCards] = useState([])
     console.log(currentUser)
 
+  //This formatdate function takes the created_at and updated_at dates and changes them into a much simpler format. This reads like the following:
+
+  // Created At: Dec 8, 2023, 07:00:00 PM
+  // Updated At: Dec 8, 2023, 07:00:00 PM
+
   const formatDate = (date) => {
     // Was having a lot of issues and couldn't tell where from, so I wrote some validations to test what could be going wrong
     let newDate = new Date(date)
+    // if not an instance of date, return null, but it should always be a date instance because of the newDate creation above
     if (!(newDate instanceof Date)) {
       console.error('Invalid date provided to formatDate:', newDate)
       return null
     }
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+    // Use options found inside of the above
     let options = {
       year: 'numeric', month: '2-digit', day: '2-digit',
       hour: '2-digit', minute: '2-digit',
@@ -27,10 +34,12 @@ function RentalAgreementsCollection({ setFromOwnerDash, fromOwnerDash, agreement
     return newDate.toLocaleDateString('en-US', options)
   }
 
+  // userSortOption, checks whether newest is selected, only other option here is oldest, and sorts by that
   let userSortOption = agreementFiltering === 'newest' ?
   ((a, b) => new Date(b.created_at) - new Date(a.created_at)) : // Newest first
   ((a, b) => new Date(a.created_at) - new Date(b.created_at)) // Oldest first
 
+  //useEffect to track the agreementFiltering 
   useEffect(() => {
     // Flatten the agreements into a single array, I needed the cart, the cart_item for quantity etc, when everything is flattened, spread it into allAgreements
     const allAgreements = role === 'user' ? (currentUser?.cart ?? []).flatMap(cart => 
@@ -46,10 +55,13 @@ function RentalAgreementsCollection({ setFromOwnerDash, fromOwnerDash, agreement
     }))
   
     // Sort all agreements based on the selected option
-    const sortedAgreements = allAgreements?.sort(userSortOption);
+    const sortedAgreements = allAgreements?.sort(userSortOption)
   
     // Map over sorted agreements to create the cards
-    
+
+    // Need to find a way to map over an array that's nested inside of an array.
+    //Went with flat map, but since there's another nested array inside of cart.cart_item, I needed to flatten that also, and finally, I map over item.agreements to get the agreement dates.
+    //Luckily with flatmap, everything was available!
     
     const cards = sortedAgreements?.map(agreement => (
       role === 'user' ? (
@@ -80,7 +92,32 @@ function RentalAgreementsCollection({ setFromOwnerDash, fromOwnerDash, agreement
           existingReviews={agreement.review}
         />
       ) : (
-        console.log(agreement)
+      <RentalAgreementCard
+        key={agreement.id}
+        rentalId={agreement.id}
+        cartName={agreement.cart_item.cart.cart_name}
+        quantity={agreement.cart_item.quantity}
+        equipmentName={agreement.cart_item.equipment.name}
+        rentalStart={formatDate(agreement.rental_start_date)}
+        rentalEnd={formatDate(agreement.rental_end_date)}
+        rentalDelivery={agreement.delivery}
+        rentalDeliveryAddress={agreement.delivery_address}
+        rentalRevisions={agreement.revisions}
+        rentalStatus={agreement.agreement_status}
+        rentalCreatedAt={agreement.created_at}
+        rentalUpdatedAt={agreement.updated_at}
+        renterFirstName={agreement.cart_item.cart.user.firstName}
+        renterLastName={agreement.cart_item.cart.user.lastName}
+        renterId = {agreement.cart_item.cart.user.id}
+        location={currentUser.location}
+        ownerEmail ={currentUser.email}
+        ownerFirstName = {currentUser.firstName}
+        ownerLastName ={currentUser.lastName}
+        ownerId = {currentUser.id}
+        setFromOwnerDash={setFromOwnerDash} 
+        fromOwnerDash={fromOwnerDash}
+        existingReviews={agreement.review}
+      />
       )
   ))
   
@@ -89,80 +126,80 @@ function RentalAgreementsCollection({ setFromOwnerDash, fromOwnerDash, agreement
   
   }, [currentUser, agreementFiltering])
   
-  let rentalCards
-  // Need to find a way to map over an array that's nested inside of an array.
-  //Went with flat map, but since there's another nested array inside of cart.cart_item, I needed to flatten that also, and finally, I map over item.agreements to get the agreement dates.
-  //Luckily with flatmap, everything was available!
-  // OLD
-  // rentalCards = currentUser?.cart?.flatMap(cart => 
+  // let rentalCards
+  // // Need to find a way to map over an array that's nested inside of an array.
+  // //Went with flat map, but since there's another nested array inside of cart.cart_item, I needed to flatten that also, and finally, I map over item.agreements to get the agreement dates.
+  // //Luckily with flatmap, everything was available!
+  // // OLD
+  // // rentalCards = currentUser?.cart?.flatMap(cart => 
+  // //   cart.cart_item?.flatMap(item => 
+  // //     item.agreements?.map(agreement=>
+
+  // if (role === 'user'){
+  // rentalCards = (currentUser?.cart ?? []).flatMap(cart => 
   //   cart.cart_item?.flatMap(item => 
-  //     item.agreements?.map(agreement=>
+  //     [...(item.agreements ?? [])].sort(userSortOption)
+  //     .map(agreement =>
+  //     <RentalAgreementCard
+  //     key={item.id}
+  //     rentalId={item.id}
+  //     cartName={cart.cart_name}
+  //     quantity={item.quantity}
+  //     equipmentName={item.equipment.name}
+  //     rentalStart={formatDate(agreement.rental_start_date)}
+  //     rentalEnd={formatDate(agreement.rental_end_date)}
+  //     rentalDelivery={agreement.delivery}
+  //     rentalDeliveryAddress={agreement.delivery_address}
+  //     rentalRevisions={agreement.revisions}
+  //     rentalStatus={agreement.agreement_status}
+  //     rentalCreatedAt={agreement.created_at}
+  //     rentalUpdatedAt={agreement.updated_at}
+  //     renterFirstName={currentUser.firstName}
+  //     renterLastName={currentUser.lastName}
+  //     renterId = {currentUser.id}
+  //     location={item.equipment.location}
+  //     ownerEmail ={item.equipment.owner.email}
+  //     ownerFirstName = {item.equipment.owner.firstName}
+  //     ownerLastName ={item.equipment.owner.lastName}
+  //     ownerId = {item.equipment.owner.id}
+  //     setFromOwnerDash={setFromOwnerDash} 
+  //     fromOwnerDash={fromOwnerDash}
+  //     existingReviews={agreement.review}
+  //     />
+  //       ) || []
+  //     ) || []
+  // )} else {
+  //   rentalCards = currentUser?.agreements?.map(agreement => 
+  //     <RentalAgreementCard
+  //     key={agreement.id}
+  //     rentalId={agreement.id}
+  //     cartName={agreement.cart_item.cart.cart_name}
+  //     quantity={agreement.cart_item.quantity}
+  //     equipmentName={agreement.cart_item.equipment.name}
+  //     rentalStart={formatDate(agreement.rental_start_date)}
+  //     rentalEnd={formatDate(agreement.rental_end_date)}
+  //     rentalDelivery={agreement.delivery}
+  //     rentalDeliveryAddress={agreement.delivery_address}
+  //     rentalRevisions={agreement.revisions}
+  //     rentalStatus={agreement.agreement_status}
+  //     rentalCreatedAt={agreement.created_at}
+  //     rentalUpdatedAt={agreement.updated_at}
+  //     renterFirstName={agreement.cart_item.cart.user.firstName}
+  //     renterLastName={agreement.cart_item.cart.user.lastName}
+  //     renterId = {agreement.cart_item.cart.user.id}
+  //     location={currentUser.location}
+  //     ownerEmail ={currentUser.email}
+  //     ownerFirstName = {currentUser.firstName}
+  //     ownerLastName ={currentUser.lastName}
+  //     ownerId = {currentUser.id}
+  //     setFromOwnerDash={setFromOwnerDash} 
+  //     fromOwnerDash={fromOwnerDash}
+  //     existingReviews={agreement.review}
+  //     />
+  //     )
+  // }
 
-  if (role === 'user'){
-  rentalCards = (currentUser?.cart ?? []).flatMap(cart => 
-    cart.cart_item?.flatMap(item => 
-      [...(item.agreements ?? [])].sort(userSortOption)
-      .map(agreement =>
-      <RentalAgreementCard
-      key={item.id}
-      rentalId={item.id}
-      cartName={cart.cart_name}
-      quantity={item.quantity}
-      equipmentName={item.equipment.name}
-      rentalStart={formatDate(agreement.rental_start_date)}
-      rentalEnd={formatDate(agreement.rental_end_date)}
-      rentalDelivery={agreement.delivery}
-      rentalDeliveryAddress={agreement.delivery_address}
-      rentalRevisions={agreement.revisions}
-      rentalStatus={agreement.agreement_status}
-      rentalCreatedAt={agreement.created_at}
-      rentalUpdatedAt={agreement.updated_at}
-      renterFirstName={currentUser.firstName}
-      renterLastName={currentUser.lastName}
-      renterId = {currentUser.id}
-      location={item.equipment.location}
-      ownerEmail ={item.equipment.owner.email}
-      ownerFirstName = {item.equipment.owner.firstName}
-      ownerLastName ={item.equipment.owner.lastName}
-      ownerId = {item.equipment.owner.id}
-      setFromOwnerDash={setFromOwnerDash} 
-      fromOwnerDash={fromOwnerDash}
-      existingReviews={agreement.review}
-      />
-        ) || []
-      ) || []
-  )} else {
-    rentalCards = currentUser?.agreements?.map(agreement => 
-      <RentalAgreementCard
-      key={agreement.id}
-      rentalId={agreement.id}
-      cartName={agreement.cart_item.cart.cart_name}
-      quantity={agreement.cart_item.quantity}
-      equipmentName={agreement.cart_item.equipment.name}
-      rentalStart={formatDate(agreement.rental_start_date)}
-      rentalEnd={formatDate(agreement.rental_end_date)}
-      rentalDelivery={agreement.delivery}
-      rentalDeliveryAddress={agreement.delivery_address}
-      rentalRevisions={agreement.revisions}
-      rentalStatus={agreement.agreement_status}
-      rentalCreatedAt={agreement.created_at}
-      rentalUpdatedAt={agreement.updated_at}
-      renterFirstName={agreement.cart_item.cart.user.firstName}
-      renterLastName={agreement.cart_item.cart.user.lastName}
-      renterId = {agreement.cart_item.cart.user.id}
-      location={currentUser.location}
-      ownerEmail ={currentUser.email}
-      ownerFirstName = {currentUser.firstName}
-      ownerLastName ={currentUser.lastName}
-      ownerId = {currentUser.id}
-      setFromOwnerDash={setFromOwnerDash} 
-      fromOwnerDash={fromOwnerDash}
-      existingReviews={agreement.review}
-      />
-      )
-  }
-
-  rentalCards.sort(userSortOption)
+  // rentalCards.sort(userSortOption)
 
     return (
     <div className="flex flex-row flex-wrap justify-start ml-6">
