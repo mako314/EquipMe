5fb5357bc9f439cd9bbd3d10ff3cc480b09c055b
Macolister <bispo.mac@gmail.com>
2023-12-13 18:54:02 -0500

    ytd total rentals and filter by month

--- client/src/OwnerComponents/OwnerDashboardComponents/RentalMonitor.js
+++ client/src/OwnerComponents/OwnerDashboardComponents/RentalMonitor.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useMemo} from "react";
 
 function RentalMonitor({currentUser}){
     
@@ -78,6 +78,7 @@ function RentalMonitor({currentUser}){
         })
     }
 
+    //my calculations aren't too expensive though. 
     let monthlyRevenueData
       if (agreementsData) {
         monthlyRevenueData = countAgreementsByMonth(agreementsData)
@@ -86,6 +87,18 @@ function RentalMonitor({currentUser}){
       } else {
         console.log('Agreements data is undefined')
       }
+    // https://react.dev/reference/react/useMemo
+
+    // const monthlyRevenueData = useMemo(() => {
+    //   if (agreementsData) {
+    //     return countAgreementsByMonth(agreementsData);
+    //   }
+    //   return [];
+    // }, [agreementsData])
+
+    // const totalRevenue = useMemo(() => {
+    //   return monthlyRevenueData.reduce((sum, { totalRevenuePerMonth }) => sum + totalRevenuePerMonth, 0)
+    // }, [monthlyRevenueData])
       
       return (
         <>
@@ -95,19 +108,43 @@ function RentalMonitor({currentUser}){
           onChange={handleMonthSelection}
         >
           <option value="" disabled>--Please choose an option--</option>
-          <option name="newest_option" value="newest" id="newest">Newest First</option>
-          <option name="oldest_option" value="oldest" id="oldest">Oldest First</option>
+          <option name="January" value="January" id="January"> January </option>
+          <option name="February" value="February" id="February"> February </option>
+          <option name="March" value="March" id="March"> March </option>
+          <option name="April" value="April" id="April"> April </option>
+          <option name="May" value="May" id="May"> May </option>
+          <option name="June" value="June" id="June"> June</option>
+          <option name="July" value="July" id="July"> July </option>
+          <option name="August" value="August" id="August"> August </option>
+          <option name="September" value="September" id="September"> September </option>
+          <option name="October" value="October" id="October"> October </option>
+          <option name="November" value="November" id="November"> November </option>
+          <option name="December" value="December" id="December"> December </option>
         </select>
         {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed 
         
         TODAY I LEARNED! I CAN MAKE MY NUMBERS PRETTY AGAIN
         */}
-          {monthlyRevenueData?.map(({ month, totalRevenuePerMonth }) => (
-            <div key={month}>
-              {month}: ${totalRevenuePerMonth.toFixed(2)}
+        
+        <div className="bg-white rounded shadow p-6 mt-4">
+            <h2 className="text-xl font-semibold text-gray-800">Total Rentals</h2>
+            <div className="mt-2">
+              {monthlyRevenueData?.map(({ month, totalRevenuePerMonth }) => (
+                monthSelection === month && (
+                  <div key={month} className="flex justify-between items-center">
+                    <span className="text-lg text-gray-500">{month}</span>
+                    <span className="text-lg font-semibold text-gray-800">${totalRevenuePerMonth.toFixed(2)}</span>
+                  </div>
+                )
+              ))}
             </div>
-          ))}
-          <div>TOTAL REVENUE: ${totalRevenue.toFixed(2)}</div>
+            <div className="border-t border-gray-200 mt-4 pt-4">
+              <div className="flex justify-between items-center">
+                <span className="text-lg text-gray-500">Year to Date</span>
+                <span className="text-lg font-semibold text-gray-800">${totalRevenue.toFixed(2)}</span>
+              </div>
+            </div>
+        </div>
         </>
       );
     }
