5bd1c8f393d7f09dd240c81aa677223b98fcb7f4
Macolister <bispo.mac@gmail.com>
2023-12-13 00:40:34 -0500

    finalize user favorited owner home portion dashboare

--- client/src/OwnerComponents/OwnerDashboardComponents/FavoritedCarousel.js
+++ client/src/OwnerComponents/OwnerDashboardComponents/FavoritedCarousel.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 
 function FavoriteCarousel({currentUser, setFromOwnerDash}) {
+
+const [currentIndex, setCurrentIndex] = useState(0)
 console.log("CAROUSEL:",currentUser)
 const navigate = useNavigate()
 
@@ -13,9 +15,10 @@ const handleEquipmentNavigation = (equipmentId) => {
   }, 0)
 }
 
+//Just did something similar as to when I had the review submission stars, but instead I'm pushing an object into carouselItems, the image has an image element 
 let carouselItems = []
 currentUser?.equipment?.map((equipment) => {
-  if(equipment?.user_favorite){
+  if(equipment?.user_favorite.length > 0){
     carouselItems.push({
     equipmentName: `${equipment?.make} ${equipment?.model}`,
     image: <img
@@ -29,24 +32,8 @@ currentUser?.equipment?.map((equipment) => {
   }
 })
 
-console.log(carouselItems)
-
-
-
-// const carouselItems = [
-//     {
-//         src: '/images/image1.jpg',
-//         alt: 'First Image',
-//     },
-//     {
-//         src: '/images/image2.jpg',
-//         alt: 'Second Image',
-//     },
-//     // Add more images as needed
-//     ]
-
-  const [currentIndex, setCurrentIndex] = useState(0)
-
+  
+  // Rather simple previous and next functions. Sets a variable to 0 to test if it's the first item in the array, from then it either adds one or removes one depending on whether or not which is being called.
   const goToPrevious = () => {
     const isFirstItem = currentIndex === 0
     const newIndex = isFirstItem ? carouselItems.length - 1 : currentIndex - 1
@@ -60,29 +47,29 @@ console.log(carouselItems)
   }
 
   return (
-      <>
-        <h2 className="text-lg font-medium text-center mb-2">User Favorites</h2>
-        <div className="flex-grow overflow-hidden w-full flex items-center justify-center"> {/* Container for the image */}
-          {carouselItems[currentIndex]?.image}
-        </div>
-        <div className="flex justify-between items-center w-full pt-4"> {/* Container for the buttons and description */}
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow"
-            onClick={goToPrevious}
-          >
-            Previous
-          </button>
-          <div className="text-center truncate px-4"> {/* Container for the image description */}
-            {carouselItems[currentIndex]?.equipmentName}
-          </div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow"
-            onClick={goToNext}
-          >
-            Next
-          </button>
-        </div>
-      </>
+    <>
+    <h2 className="text-lg font-medium text-center mb-2 bg-gray-100 rounded-lg shadow-lg ">User Favorites</h2>
+    <div className="flex-grow overflow-hidden w-full flex items-center justify-center bg-gray-100 rounded-lg shadow-lg pt-4"> {/* Container for the image */}
+      {carouselItems[currentIndex]?.image}
+    </div>
+    <div className="flex justify-between items-center w-full pt-4 px-4 bg-gray-100 rounded-lg shadow-lg py-2"> {/* Container for the buttons and description */}
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded shadow"
+        onClick={goToPrevious}
+      >
+        Previous
+      </button>
+      <div className="text-center truncate px-4 py-1 bg-white rounded-lg shadow"> {/* Container for the image description */}
+        {carouselItems[currentIndex]?.equipmentName}
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded shadow"
+        onClick={goToNext}
+      >
+        Next
+      </button>
+    </div>
+  </>
   )
 }
 
