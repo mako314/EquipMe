eea3748c61a0e38244680dbf8213419f333f749f
Macolister <bispo.mac@gmail.com>
2023-12-13 01:57:15 -0500

    temp svg for error page

--- client/src/ErrorPageComponents/Page404.js
+++ client/src/ErrorPageComponents/Page404.js
@@ -21,9 +21,14 @@ function Page404({errorString}){
         <div class="container flex items-center justify-center min-h-screen px-6 py-12 mx-auto">
             <div class="w-full ">
                 <div class="flex flex-col items-center max-w-lg mx-auto text-center">
+                     <p class="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800 mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                        </svg>
+                    </p>
                     <p class="text-sm font-medium text-blue-500 dark:text-blue-400"> We've Run into An Error! </p>
                     <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">{errorString}</h1>
-                    <p class="mt-4 text-gray-500 dark:text-gray-400">We searched high and low, but couldn’t find what you’re looking for.Let’s find a better place for you to go.</p>
+                    <p class="mt-4 text-gray-500 dark:text-gray-400">We searched high and low, but couldn’t find what you’re looking for. Let’s find a better place for you to go.</p>
 
                     <div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
                         <button class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg dark:text-gray-200 gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:border-gray-700">
