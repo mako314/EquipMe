955169808538d72b0587e7b4e8d275586b82330b
Macolister <bispo.mac@gmail.com>
2023-12-14 20:09:09 -0500

    seeding for equipment state_history

--- server/models.py
+++ server/models.py
@@ -220,7 +220,7 @@ class Equipment(db.Model, SerializerMixin):
     state_history = db.relationship('EquipmentStateHistory', back_populates='equipment')
 
     #Serialization rules
-    serialize_rules = ('-owner.equipment','-owner.owner_inboxes','-owner.agreements', '-owner.owner_favorite','-owner.review','-owner.user_favorite','-images.equipment', '-cart_item.equipment','-equipment_price.equipment', '-featured_equipment.equipment','-cart_item.review','-cart_item.agreements', '-cart_item.cart', '-user_favorite.equipment', '-user_favorite.owner', '-user_favorite.user.user_inboxes', '-user_favorite.user.agreement','-user_favorite.user.cart','-user_favorite.user.review')
+    serialize_rules = ('-owner.equipment','-owner.owner_inboxes','-owner.agreements', '-owner.owner_favorite','-owner.review','-owner.user_favorite','-images.equipment', '-cart_item.equipment','-equipment_price.equipment', '-featured_equipment.equipment','-cart_item.review','-cart_item.agreements', '-cart_item.cart', '-user_favorite.equipment', '-user_favorite.owner', '-user_favorite.user.user_inboxes', '-user_favorite.user.agreement','-user_favorite.user.cart','-user_favorite.user.review', '-state_history.equipment')
     
     # '-agreements.equipment', # REMOVED DUE TO AGREEMENTS BEING TO CART ITEMS
     
@@ -285,17 +285,26 @@ class EquipmentStateHistory(db.Model, SerializerMixin):
     changed_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     equipment = db.relationship('Equipment', back_populates='state_history')
+    # equipment_state_summary = db.relationship('EquipmentStateSummary', back_populates='state_history')
 
     serialize_rules = ('-equipment.state_history', )
 
 
 class EquipmentStateSummary(db.Model, SerializerMixin):
-    __tablename__ = "equipment_state_summary"
+    __tablename__ = "equipment_state_summaries"
     
     id = db.Column(db.Integer, primary_key=True)
+    equipment_history_id = db.Column(db.Integer, db.ForeignKey('equipment_state_history.id'))
     date = db.Column(db.Date)
     state = db.Column(db.String)
     total_quantity = db.Column(db.Integer, default=0)
+    total_idle = db.Column(db.Integer, default=0)
+    total_rented_out = db.Column(db.Integer, default=0)
+
+    # state_history = db.relationship('EquipmentStateHistory', back_populates='equipment_state_summary')
+
+    # serialize_rules = ('-equipment_state_summary.state_history', )
+
 
 
 class FeaturedEquipment(db.Model, SerializerMixin):
--- server/seeds.py
+++ server/seeds.py
@@ -1,4 +1,4 @@
-from models import db, User, EquipmentOwner, Equipment, RentalAgreement, EquipmentImage, Thread, UserInbox, OwnerInbox, Message, Cart, CartItem, EquipmentPrice, FeaturedEquipment, Review, UserFavorite, OwnerFavorite, AgreementComment
+from models import db, User, EquipmentOwner, Equipment, RentalAgreement, EquipmentImage, Thread, UserInbox, OwnerInbox, Message, Cart, CartItem, EquipmentPrice, FeaturedEquipment, Review, UserFavorite, OwnerFavorite, AgreementComment, EquipmentStateHistory, EquipmentStateSummary
 import pandas as pd
 from app import app
 from random import randint, choice as rc
@@ -175,6 +175,7 @@ if __name__ == '__main__':
         db.session.add(user_4)
         db.session.add(user_5)
         db.session.add(user_6)
+        db.session.add_all([user_1,user_2,user_3,user_4,user_5,user_6])
 
 
         db.session.commit()
@@ -357,16 +358,17 @@ if __name__ == '__main__':
         owner_10.password_hash = owner_10_password
 
         #Re-add all owners
-        db.session.add(owner_1)
-        db.session.add(owner_2)
-        db.session.add(owner_3)
-        db.session.add(owner_4)
-        db.session.add(owner_5)
-        db.session.add(owner_6)
-        db.session.add(owner_7)
-        db.session.add(owner_8)
-        db.session.add(owner_9)
-        db.session.add(owner_10)
+        db.session.add_all([owner_1,owner_2,owner_3,owner_4,owner_5,owner_6,owner_7,owner_8,owner_9,owner_10,])
+        # db.session.add(owner_1)
+        # db.session.add(owner_2)
+        # db.session.add(owner_3)
+        # db.session.add(owner_4)
+        # db.session.add(owner_5)
+        # db.session.add(owner_6)
+        # db.session.add(owner_7)
+        # db.session.add(owner_8)
+        # db.session.add(owner_9)
+        # db.session.add(owner_10)
 
         db.session.commit()
 
@@ -444,6 +446,49 @@ if __name__ == '__main__':
 
         db.session.add_all(equipment_list)
         db.session.commit()
+#--------------------------------------------Equipment State History----------------------------------------------------------------------
+        print('Adding Equipment State History...')
+
+        equipment_state_history_1 = EquipmentStateHistory(
+            equipment_id = equipment_list[0].id,  # Excavator
+            previous_quantity = 0,
+            new_quantity = 3,
+            previous_state = 'non-existing',
+            new_state = 'added',
+            changed_at = datetime.utcnow(),
+        )
+
+        equipment_state_history_2 = EquipmentStateHistory(
+            equipment_id = equipment_list[1].id,  # Forklift
+            previous_quantity = 0,
+            new_quantity = 2,
+            previous_state = 'non-existing',
+            new_state = 'added',
+            changed_at = datetime.utcnow(),
+        )
+
+        equipment_state_history_3 = EquipmentStateHistory(
+            equipment_id = equipment_list[2].id,  # Lawnmower
+            previous_quantity = 0,
+            new_quantity = 5,
+            previous_state = 'non-existing',
+            new_state = 'added',
+            changed_at = datetime.utcnow(),
+        )
+
+        equipment_state_history_4 = EquipmentStateHistory(
+            equipment_id = equipment_list[3].id,  # Tractor
+            previous_quantity = 0,
+            new_quantity = 4,
+            previous_state = 'non-existing',
+            new_state = 'added',
+            changed_at = datetime.utcnow(),
+        )
+
+        db.session.add_all([equipment_state_history_1, equipment_state_history_2, equipment_state_history_3, equipment_state_history_4])
+        db.session.commit()
+
+        
 #------------------------------------------EQUIPMENT PRICES---------------------------------------
         print('Calculating price of Equipment...')
         equipment_prices = [
