eb6537c0e3bff6489e5333b708c98cea6dfbbe07
Macolister <bispo.mac@gmail.com>
2023-12-27 14:14:44 -0500

    fix maintenance quantity bug

--- client/src/ChartComponents/BarChartDataContext.js
+++ client/src/ChartComponents/BarChartDataContext.js
@@ -6,6 +6,7 @@ export const useEquipmentData = () => useContext(BarChartEquipmentData)
 
 export const EquipmentDataProvider = ({ children }) => {
   const [barChartEquipmentData, setBarChartEquipmentData] = useState([])
+  
 
   return (
     <BarChartEquipmentData.Provider value={{ barChartEquipmentData, setBarChartEquipmentData }}>
