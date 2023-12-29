bb25ca4854b54997509de29a6c8e8d0097fb7967
Macolister <bispo.mac@gmail.com>
2023-12-29 18:01:19 -0500

    remove user / owner context from all,

--- client/src/MessagingComponents/ContactModal.js
+++ client/src/MessagingComponents/ContactModal.js
@@ -1,13 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react'
-// import OwnerContext from '../OwnerComponents/OwnerContext'
-// import UserContext from '../UserComponents/UserContext'
 import ApiUrlContext from '../Api'
 import {toast} from 'react-toastify'
 import { UserSessionContext } from '../UserComponents/SessionContext'
 
 function ContactModal({recipientID}){
-    // const [owner, setOwner] = useContext(OwnerContext)
-    // const [user, setUser] = useContext(UserContext)
     const { currentUser, role, checkSession } = UserSessionContext()
 
     const apiUrl = useContext(ApiUrlContext)
--- client/src/OwnerComponents/OwnerDashboardComponents/OwnerDashboard.js
+++ client/src/OwnerComponents/OwnerDashboardComponents/OwnerDashboard.js
@@ -281,7 +281,9 @@ function AccountSettings() {
                 <div className="flex-grow p-6 overflow-auto bg-gray-200">
                     <div className="grid grid-cols-3 gap-6">
                         
-                        <div className="h-24 col-span-3 bg-white border border-gray-300 text-center"> Welcome {currentUser.firstName} </div>
+                        <div className="h-24 col-span-3 bg-white border border-gray-300 text-center flex items-center justify-center"> 
+                        <h1 className="text-2xl font-semibold text-black">Welcome, {currentUser.firstName}</h1>
+                        </div>
 
                         <div className="h-96 py-2 col-span-1 bg-white border border-gray-300">
                         
@@ -460,7 +462,9 @@ function AccountSettings() {
                             </>
                             }
 
-                            <button className="relative ml-2 text-sm focus:outline-none group">
+
+                            {/* DROP DOWN THINGY */}
+                            {/* <button className="relative ml-2 text-sm focus:outline-none group">
                                 <div className="flex items-center justify-between w-10 h-10 rounded hover:bg-gray-300">
                                     <svg className="w-5 h-5 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
@@ -471,7 +475,10 @@ function AccountSettings() {
                                     <span className="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</span>
                                     <span className="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</span>
                                 </div>
-                            </button>
+                            </button> */}
+
+
+
                         </div>
                         {renderCurrentView()}
                     </div>
