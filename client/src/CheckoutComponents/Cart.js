3a91430324ff9edee5659ffae8102608b53e76ed
Macolister <bispo.mac@gmail.com>
2023-12-29 18:01:29 -0500

    tofixed in cart total

--- client/src/CheckoutComponents/Cart.js
+++ client/src/CheckoutComponents/Cart.js
@@ -153,7 +153,7 @@ function Cart(){
         <div className="flex justify-between">
           <p className="text-lg font-bold">Total</p>
           <div className="">
-            <p className="mb-1 text-lg font-bold">${(cartData[currentCart]?.total / 100)}</p>
+            <p className="mb-1 text-lg font-bold">${(cartData[currentCart]?.total / 100).toFixed(2)}</p>
             <p className="text-sm text-gray-700">including VAT</p>
           </div>
         </div>
