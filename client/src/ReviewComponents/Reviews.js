43e1c680e8073dd1c8cab00d63eb1c9bdd1a8f0c
Macolister <bispo.mac@gmail.com>
2024-01-22 18:12:52 -0500

    i had cart instead of owner_inboxes for owner inbox delete

--- client/src/ReviewComponents/Reviews.js
+++ client/src/ReviewComponents/Reviews.js
@@ -56,52 +56,48 @@ function Reviews({stars, comment, image, firstName, lastName, profession, review
     </div> */}
 
     return(
-      <div className="grid border border-solid border-[#dfdfdf] bg-white p-8 md:p-10">
-        <div className="flex">
-            {starDisplayer}
-            <div className="flex justify-end">
-                  <button
-                    onClick={handleToggleDelete}
-                    className="ml-4" 
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-
-                  </button>
-
-                  {toggleDelete && (
-                  <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-40 overflow-y-auto h-full w-full" onClick={() => setToggleDelete(false)}>
-                    <div className="relative top-20 mx-auto p-5 border w-1/3 shadow-lg rounded-md bg-white">
-                      <h3 className="text-lg font-medium leading-6 text-gray-900">Are you sure you want to delete this item?</h3>
-                      <div className="mt-2">
-                        <button
-                          onClick={() => handleDeleteReview(reviewId)}
-                          className="mr-2 rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600"
-                        >
-                          Yes, Remove This Review.
-                        </button>
-                        <button
-                          onClick={handleToggleDelete}
-                          className="rounded bg-gray-500 py-2 px-4 text-white hover:bg-gray-600"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                )}
-            </div>
-        </div>
-        <p className="text-[#647084] mb-4">{comment}</p>
-        <div className="flex mt-auto">
-          <img src={image} alt="" className="mr-4 h-16 w-16 rounded-full" />
-          <div className="flex flex-col justify-center">
-            <h6 className="font-bold">{firstName} {lastName}</h6>
-            <p className="text-sm text-[#636262]">{profession}</p>
-          </div>
+<div className="grid border border-solid border-[#dfdfdf] bg-white p-8 md:p-10">
+  <div className="flex justify-between mb-4">
+    <div className="flex"> {/* Ensure this div has `flex` to layout stars horizontally */}
+      {starDisplayer}
+    </div>
+    <button onClick={handleToggleDelete} className="ml-4 self-start">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+      </svg>
+    </button>
+  </div>
+  <p className="text-[#647084]">{comment}</p>
+  <div className="flex mt-4">
+    <img src={image} alt="" className="mr-4 h-16 w-16 rounded-full" />
+    <div className="flex flex-col justify-center">
+      <h6 className="font-bold">{firstName} {lastName}</h6>
+      <p className="text-sm text-[#636262]">{profession}</p>
+    </div>
+  </div>
+  {toggleDelete && (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-40 overflow-y-auto h-full w-full" onClick={() => setToggleDelete(false)}>
+      <div className="relative top-20 mx-auto p-5 border w-1/3 shadow-lg rounded-md bg-white">
+        <h3 className="text-lg font-medium leading-6 text-gray-900">Are you sure you want to delete this item?</h3>
+        <div className="mt-2">
+          <button
+            onClick={() => handleDeleteReview(reviewId)}
+            className="mr-2 rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600"
+          >
+            Yes, Remove This Review.
+          </button>
+          <button
+            onClick={handleToggleDelete}
+            className="rounded bg-gray-500 py-2 px-4 text-white hover:bg-gray-600"
+          >
+            Cancel
+          </button>
         </div>
       </div>
+    </div>
+  )}
+</div>
+
     )
 }
 
